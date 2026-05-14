ed18f02d4d7273745c540821b15c93ff5974d6dc
Tiger_CDev <tigerchoopaibool@gmail.com>
2026-05-15 02:47:53 +0700

    implement delete ingredient.

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -92,4 +92,4 @@ class PrivateIngredientsApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
         ingredients = Ingredient.objects.filter(user=self.user)
-        self.assertFalse(ingredient.exists())
+        self.assertFalse(ingredients.exists())

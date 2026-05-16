86d8d8dff697bb85c799e9fafbc6c6b7b7f99885
Tiger_CDev <tigerchoopaibool@gmail.com>
2026-05-17 02:19:29 +0700

    implement image API

--- app/app/settings.py
+++ app/app/settings.py
@@ -109,7 +109,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -142,4 +141,8 @@ AUTH_USER_MODEL = 'core.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'COMPONENT_SPLIT_REQUEST': True,
 }
